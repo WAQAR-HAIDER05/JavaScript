748684ba7b412651ea4a88f299b963b00176f3f8
Waqar Haider <107320775+WAQAR-HAIDER05@users.noreply.github.com>
2025-04-09 17:57:45 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId= 1234567
+let accountEmail="haiderwaqar@gmail.com"
+var accountPassword= "1212"
+countryName= "Pakistan"
+
+// accountId =2  cnatnot chnaged
+accountEmail="waqarhaider@hamajd"
+accountPassword="2222"
+countryName="Iran"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,countryName])
+
+
+/*prefer to not use var keyword
+because of issue in block scope and functional scope 
+*/
